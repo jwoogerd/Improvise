a04a720a3d9cbe173328644047660c8f7b232423
Caroline Marcks <caroline.marcks@tufts.edu>
2013-11-11 15:29:32 -0500

    added game file

---
+++ MusicGames/MG.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleContexts, TypeFamilies #-}
+
+
+import Control.Monad.Trans (liftIO)
+import Data.List           (elemIndices, intersperse, transpose)
+import Text.Printf         (printf)
+import Hagl
+import Euterpea
+
+range = 5
+
+data RMove = Begin Pitch
+           | Rest
+           | Extend
+           -- invariant: Extend implies extending a previous "Begin"
+
+start :: [[RMove]]
+start = [[Begin (C, 5), Main.Rest, Begin (D, 5)], [Begin (A, 5), Extend, Main.Rest]]
+-- find a way to assert that inner lists are the same length
+
+possMoves :: [RMove] -> [RMove]
+possMoves l@((Begin p):prev) = rangedMoves l ++ [Main.Rest, Extend]
+possMoves l                  = rangedMoves l
+
+rangedMoves :: [RMove] -> [RMove]
+rangedMoves ((Begin p) : prev) = upperMoves p range ++ lowerMoves p range
+rangedMoves (_         : prev) = rangedMoves prev
+rangedMoves []                 = [Main.Rest]
+
+
+upperMoves :: Pitch -> Int -> [RMove]
+upperMoves p i = case i of 0 -> []
+                           _ -> let u = halfStepUp p 
+                                in  Begin u : upperMoves u (i - 1)
+ 
+lowerMoves :: Pitch -> Int -> [RMove]
+lowerMoves p i = case i of 0 -> []
+                           _ -> let u = halfStepDown p 
+                                in  Begin u : lowerMoves u (i - 1)
+
+halfStepDown p = (C, 1)
+halfStepUp p = (C, 1)
+  --temporary
+ 
+
+main = do { putStrLn "yo" }
+
