f2971c4dbecbd9757de454a6b86967f073ee4020
Caroline Marcks <caroline.marcks@tufts.edu>
2013-11-11 18:36:29 -0500

    JUST MG

--- MusicGames/MG.hs
+++ MusicGames/MG.hs
@@ -20,12 +20,12 @@ start = [[Begin (C, 5), Main.Rest, Begin (D, 5)], [Begin (A, 5), Extend, Main.Re
 -- find a way to assert that inner lists are the same length
 
 type Player = Int
-type RealizationState = [([RMoves], [RMoves])]
+type RealizationState = [([RMove], [RMove])]
   -- A list of tuples, each one representing a player
      -- their first list represents the realization of their score at this point in the game
      -- their second list represents the remainder of the score
 
-progress :: RealizationState -> [RMoves] -> RealizationState
+progress :: RealizationState -> [RMove] -> RealizationState
 progress []              []       = []
 progress ((r,(_:sc)):ps) (mv:mvs) = (mv:r,sc) : (progress ps mvs)
 
@@ -50,48 +50,23 @@ lowerMoves p i = case i of 0 -> []
                            _ -> let u = halfStepDown p 
                                 in  Begin u : lowerMoves u (i - 1)
  
-
-type Improvise 
-instance IsSimultaneous Improvise RMove where
-  toSimultaneous :: Improvise -> Simultaneous RMove
-  toSimultaneous 
-
-  Simultaneous RMove = Simultaneous Int (PlayerID -> RMove -> Bool) (Profile RMove -> Payoff)
-
-
-type Improvise = Imp (Simultaneous RMove) RealizationState -- State?
+data Improvise = Imp (Simultaneous RMove) RealizationState 
 instance Game Improvise where
   
-  type TreeType Improvise = Discrete
+  type TreeType Improvise = Continuous
   type State    Improvise = RealizationState
   type Move     Improvise = RMove
 
-  gameTree :: Improvise -> (TreeType Improvise) (State Improvise) (Move Improvise) 
-  gameTree (Imp (Simultaneous numPlayers validMove f) RS) = tree 1 []
+  gameTree (Imp (Simultaneous numPlayers validMove f) rs) = tree 1 []
     where
       tree p ms
-        | p <= numPlayers = Continuous (RS, Decision p)
+        | p <= numPlayers = Continuous (rs, Decision p)
                             (\rmove -> if validMove p rmove
                                        then Just (tree (p+1) (rmove:ms))
                                        else Nothing)
-        | otherwise       = let newState = progress RS (reverse ms)
+        | otherwise       = let newState = progress rs (reverse ms)
                             in Continuous (newState, (Payoff . f . ByPlayer . reverse) ms)
                             (\_ -> Nothing)
                             
--- | Build a discrete game tree for a state-based game.
-stateTreeD ::
-        (s -> PlayerID) -- who   ^ Whose turn is it?
-     -> (s -> Bool)     -- end   ^ Is the game over?
-     -> (s -> [mv])     -- moves ^ Available moves.
-     -> (s -> mv -> s)  -- exec  ^ Execute a move and return the new state.
-     -> (s -> Payoff)   -- pay   ^ Payoff for this (final) state.
-     -> s               -- s     ^ The current state.
-     -> Discrete s mv
-stateTreeD who end moves exec pay = tree
-  where tree s | end s     = Discrete (s, Payoff (pay s)) []
-               | otherwise = Discrete (s, Decision (who s)) [(m, tree (exec s m)) | m <- moves s]
-
-
-
 main = do { putStrLn "yo" }
 
