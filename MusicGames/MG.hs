6ed6256ded2f404dfa4a8698b005d6a06f9cf09f
Caroline Marcks <caroline.marcks@tufts.edu>
2013-11-16 13:04:43 -0500

    state tree d working... need to fix possmoves

--- MusicGames/MG.hs
+++ MusicGames/MG.hs
@@ -16,30 +16,52 @@ data RMove = Begin Pitch
            | Extend deriving (Show, Eq)
            -- invariant: Extend implies extending a previous "Begin"
 
-type RealizationState = [([RMove], [RMove])]
-  -- A list of tuples, each one representing a player
-     -- their first list represents the realization of their score at this point in the game
-     -- their second list represents the remainder of the score
+data SingularScore = SS { realization :: [RMove], future :: [RMove] } deriving Show
+data RealizationState = RS { scores :: [SingularScore], accumulating :: [RMove] } deriving Show
+
+
+player1 :: SingularScore
+player1 = SS { realization = [], future = [Begin (C,5), Main.Rest, Begin (D,5)] }
+player2 :: SingularScore
+player2 = SS { realization = [], future = [Begin (A,5), Extend, Main.Rest] }
 
 start :: RealizationState
-start = [([],[Begin (C,5), Main.Rest, Begin (D,5)]),([],[Begin (A,5), Extend, Main.Rest])]
---start = [([],[Begin (C,5)]),([],[Begin (A,5)])]
--- find a way to assert that inner lists are the same length
+start = RS { scores = [player1,player2] , accumulating = [] }
 
-progress :: RealizationState -> [RMove] -> RealizationState
-progress []              []       = []
-progress ((r, _:sc):ps) (mv:mvs) = (mv:r,sc) : progress ps mvs
 
-possMoves :: [RMove] -> [RMove]
-possMoves m@(Begin p:prev) = rangedMoves m ++ [Main.Rest, Extend]
-possMoves                m = rangedMoves m
+registerMove :: RealizationState -> RMove -> RealizationState
+registerMove rs mv = let newRS = RS { scores = (scores rs), 
+                                      accumulating = (mv : (accumulating rs))}
+                      in if (length (accumulating newRS) == length (scores newRS)) 
+                         then progress newRS
+                         else newRS
+
+progress :: RealizationState -> RealizationState
+progress rs = let newPlayers = progressHelper (scores rs) (reverse (accumulating rs))
+               in RS {scores = newPlayers, accumulating = []}
+
+
+progressHelper :: [SingularScore] -> [RMove] -> [SingularScore]
+progressHelper [] [] = []
+progressHelper (p:ps) (mv:mvs) = (SS { realization = mv:(realization p), 
+                                       future      = (drop 1 (future p))})
+                                 : (progressHelper ps mvs)
+
+markable :: RealizationState -> [RMove]
+markable rs = possMoves ( (scores rs) !! (length (accumulating rs)))
+
+possMoves :: SingularScore -> [RMove]
+possMoves SS { realization = m, future = (Begin p):ps } = generateMoves p ++ [Main.Rest, Extend]
+possMoves SS { realization = m, future = _ } = rangedMoves m ++ [Main.Rest]
 
 
 rangedMoves :: [RMove] -> [RMove]
 rangedMoves ((Begin p) :prev) = generateMoves p
 rangedMoves (       _  :prev) = rangedMoves prev
-rangedMoves                 _ = [Main.Rest]                            
+rangedMoves                 _ = []                            
 
+who :: RealizationState -> PlayerID
+who rs = length (accumulating rs)
 
 
 -- returns a list of RMoves range number of halfsteps above & below p
@@ -48,54 +70,38 @@ generateMoves p =
     let genMoves _ _ 0 = []
         genMoves p f n = let m = f p
                          in Begin m : genMoves m f (n-1)
-    in genMoves p halfStepUp range ++ genMoves p halfStepDown range
+    in genMoves p halfStepUp range ++ [Begin p] ++ genMoves p halfStepDown range
+
 
 end :: RealizationState -> Bool
-end rs@((_,[]):_) = trace ("True " ++ show rs) True
-end rs          = trace ("False " ++ show rs) False
+end rs = null (accumulating rs) && null (future (head (scores rs)))
 
-data Improvise = Imp (Simultaneous RMove) RealizationState 
+x :: Float
+x = 1.0
+pay :: RealizationState -> Payoff
+pay rs = ByPlayer (take (length (scores rs)) (repeat x))
+
+data Improvise = Improvise
+
+-- Game instance
 instance Game Improvise where
-  
-  type TreeType Improvise = Continuous
-  type State    Improvise = RealizationState
-  type Move     Improvise = RMove
-
-  gameTree t@(Imp (Simultaneous numPlayers validMove f) rs) = trace ("YO " ++ show rs) (tree 1 [])
-    where
-      tree p ms
-        | p <= numPlayers = Continuous (rs, Decision p)
-                            (\rmove -> if trace ("I AM PLAYER " ++ show p) (validMove p rmove)
-                                       then Just (tree (p+1) (rmove:ms))
-                                       else Nothing)
-        | otherwise       = let newState = progress rs (reverse ms)
-                            in trace "TOO MANY PLAYERS, YO" (Continuous (newState, (Payoff . f . ByPlayer . reverse) ms)
-                            (\_ -> if end newState
-                                   then Nothing
-                                   else Just (gameTree (Imp (Simultaneous numPlayers validMove f) newState))))
-getPayoff:: Profile RMove -> Payoff
-getPayoff (ByPlayer _) = ByPlayer [1,0]
-
-
-testGame = Imp (Simultaneous 2 (\x -> \y -> True) Main.getPayoff) start
-
-guessPlayers :: [Hagl.Player Improvise]
-guessPlayers = ["A" ::: return Main.Rest, "B" ::: return Main.Rest]
-                            
-execute = evalGame testGame guessPlayers (run >> printTranscript)
-   where run = printGame >> step >>= maybe run (\p -> printGame >> return p)
-
---playTicTacToe = evalGame guessPlayers
---                         (run >> printScore)
---  where run = printGame >> step >>= maybe run (\p -> printGame >> return p)
-
- 
+  type TreeType Improvise = Discrete
+  type Move  Improvise = RMove
+  type State Improvise = RealizationState
+  gameTree _ = stateTreeD who end markable registerMove pay start
 
 printGame :: GameM m Improvise => m ()
 printGame = gameState >>= liftIO . putStrLn . show
+
+guessPlayers :: [Hagl.Player Improvise]
+guessPlayers = ["A" ::: return Main.Rest, "B" ::: return Main.Rest]
+
+execute = evalGame Improvise guessPlayers (run >> printTranscript)
+   where run = printGame >> step >>= maybe run (\p -> printGame >> return p)
+
+
 
 --octv :: Octave
 --octv = 5
 --main = do { putStrLn "Just MG" ;
 --            Euterpea.play (Prim (Note 1 (Ass, octv)))}
-
