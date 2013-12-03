84a52183ace2fba5fd7c5a86a17ed3f9d9aa0bff
Jayme Woogerd <jayme.woogerd@tufts.edu>
2013-12-03 12:30:44 -0500

    Add Range type and rename rangedMoves

--- MusicGames/Moves.hs
+++ MusicGames/Moves.hs
@@ -14,28 +14,30 @@ improvise, but also increases the size of the game tree.
 
 -}
 
+type Range = Int
+
 -- | Generate a list of possible moves from a given range and score. We  
 -- enforce the following invariants for possible moves:
 --  TODO: nail down invariants
-possMoves :: Int -> SingularScore -> [RMove]
-possMoves i ss = case ss of 
+availableMoves :: Range -> SingularScore -> [RMove]
+availableMoves i ss = case ss of 
   (SS _               [])          -> []
   (SS m@(Begin r:rs) (Begin f:fs)) -> Rest: Extend r: 
-                                            rangedMoves i m ++ generateMoves i f
-  (SS m              (Begin f:fs)) -> Rest: rangedMoves i m ++ generateMoves i f
-  (SS m@(Begin r:rs)  _ )          -> Rest: Extend r: rangedMoves i m
-  (SS m@(Extend r:rs) _ )          -> Rest: Extend r: rangedMoves i m
-  (SS m               _ )          -> Rest:           rangedMoves i m
+                                            fromPast i m ++ generateMoves i f
+  (SS m              (Begin f:fs)) -> Rest: fromPast i m ++ generateMoves i f
+  (SS m@(Begin r:rs)  _ )          -> Rest: Extend r: fromPast i m
+  (SS m@(Extend r:rs) _ )          -> Rest: Extend r: fromPast i m
+  (SS m               _ )          -> Rest:           fromPast i m
 
--- | Produce a list of moves from the most recent past musical event.
-rangedMoves :: Int -> [RMove] -> [RMove]
-rangedMoves r (Begin p:prev) = generateMoves r p
-rangedMoves r (_      :prev) = rangedMoves r prev
-rangedMoves _ _              = []                            
+-- | Produce a list of moves from the most recent past Begin.
+fromPast :: Range -> [RMove] -> [RMove]
+fromPast r (Begin p:prev) = generateMoves r p
+fromPast r (_      :prev) = fromPast r prev
+fromPast _ _              = []                            
 
 -- | For a given range and pitch, generate a list of moves (Begins) range 
 -- number of half steps above and below that pitch.
-generateMoves :: Int -> Pitch -> [RMove]
+generateMoves :: Range -> Pitch -> [RMove]
 generateMoves range p =
     let genMoves _ _ 0 = []
         genMoves p f n = let m = f p
