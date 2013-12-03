6b30882e910649a975f0aaafecae2e355eb459c1
Jayme Woogerd <jayme.woogerd@tufts.edu>
2013-12-03 15:34:16 -0500

    Clean up code and add documentation to Game.hs

--- MusicGames/Game.hs
+++ MusicGames/Game.hs
@@ -1,20 +1,31 @@
 {-# LANGUAGE FlexibleContexts, TypeFamilies #-}
+
 module Game where
+
 import Euterpea
 import Hagl
 import State
 import Moves
 
+--
+-- * Game representation 
+--
 
+-- ** These functions are common to all Improvise game executions.
+
+-- | Whose turn it is.
 who :: RealizationState -> PlayerID
 who rs = length (accumulating rs) + 1
 
+-- | True when the game is over. 
 end :: RealizationState -> Bool
 end (RS scores accumulating) = null accumulating && null (future (head scores))
 
+-- | Get a list of the available moves given the allowed range and score.
 playable :: Range -> RealizationState -> [RMove]
 playable r rs = availableMoves r $ scores rs !! length (accumulating rs)
-  
+
+-- | A player makes a move.
 registerMove :: RealizationState -> RMove -> RealizationState
 registerMove rs mv = if length (accumulating newRS) == length (scores newRS)
                      then progress newRS
@@ -24,13 +35,16 @@ registerMove rs mv = if length (accumulating newRS) == length (scores newRS)
                             newPlayers = zipWith step (scores rs) (reverse $ accumulating rs)
                         in RS newPlayers []
 
-
-
+-- | The Improvise data type wraps information unique to a particular game 
+-- execution: a description of the players' musical aesthetic preferences (i.e. 
+-- a function to generate payoffs); the game state (the scores); and the range 
+-- of allowed deviation from the score for this game execution.
 data Improvise = Imp { payoff :: RealizationState -> Payoff
                      , state  :: RealizationState
                      , range  :: Range}
     
--- Game instance
+
+-- | Game instance
 instance Game Improvise where
   type TreeType Improvise = Discrete
   type Move  Improvise = RMove
