3fcff8ff84539a189690558e9ab8c3257bf9c04b
Jayme Woogerd <jayme.woogerd@tufts.edu>
2013-12-11 16:42:29 -0500

    Refactor and clean up Payoff.hs
    Improve IntervalPayoff and associated functions with more accurate
    documentation and more intermediately named values.
    
    Also remove unnecessary includes.

--- Improvise/Payoff.hs
+++ Improvise/Payoff.hs
@@ -5,9 +5,9 @@ module Payoff where
 import Game
 import Hagl
 import Euterpea (absPitch, Pitch)
-import Control.Monad (liftM)
-import Data.Function (on)
-import Data.List     (maximumBy, sortBy)
+
+import Data.Maybe (fromMaybe)
+import Data.List  (lookup)
 
 {- 
 
@@ -26,12 +26,13 @@ notion of each players' musical aesthetic preferences.
 -- | A musical interval, an integer difference between two pitches.
 type Interval = Int
 
--- | An interval preference is represented as the interval and the payoff the
--- player would recieve by playing it. Positive payoffs denote favorable
--- intervals, negative payoffs signify undesirable ones.
+-- | An interval preference is an association list of musical intervals and 
+-- the payoff the player would receive by playing it. Positive payoffs denote 
+-- favorable intervals, negative payoffs signify undesirable ones.
 type IntPreference = (Interval, Float)
 
--- | Compute the integer difference between two pitches, from p1 to p2. Full octaves spanned are ignored. Returns -12 to 12.
+-- | Compute the integer difference between two pitches, from p1 to p2. Full 
+-- octaves spanned are ignored. Returns -12 to 12.
 interval :: Pitch -> Pitch -> Int
 interval p1 p2 =
   let pitch1 = absPitch p1
@@ -40,41 +41,41 @@ interval p1 p2 =
      then (pitch2 `mod` 12) - (pitch1 `mod` 12)
      else (pitch2 `mod` 24) - (pitch1 `mod` 12)
 
--- | Compute the integer difference between two pitches, from p1 to p2. Full octaves spanned are included.
+-- | Compute the integer difference between two pitches, from p1 to p2. Full 
+-- octaves spanned are included.
 interval8ve :: Pitch -> Pitch -> Int
 interval8ve p1 p2 = absPitch p2 - absPitch p1
 
--- | Sum the total payoff for a player, given his preferences and an interval.
-intPref :: [IntPreference] -> Int -> Float
-intPref prefs i = foldr f 0 prefs
-    where f (interval, pay) acc = if i == interval then pay + acc else acc
-
 -- | Find the interval between two moves, if one exists.
-rmoveInterval :: MusicMv -> MusicMv -> Maybe Interval
-rmoveInterval (Begin p1)  (Begin p2)  = Just $ interval p1 p2
-rmoveInterval (Begin p1)  (Extend p2) = Just $ interval p1 p2
-rmoveInterval (Extend p1) (Begin p2)  = Just $ interval p1 p2
-rmoveInterval (Extend p1) (Extend p2) = Just $ interval p1 p2
-rmoveInterval _           _           = Nothing
+moveInterval :: MusicMv -> MusicMv -> Maybe Interval
+moveInterval (Begin p1)  (Begin p2)  = Just $ interval p1 p2
+moveInterval (Begin p1)  (Extend p2) = Just $ interval p1 p2
+moveInterval (Extend p1) (Begin p2)  = Just $ interval p1 p2
+moveInterval (Extend p1) (Extend p2) = Just $ interval p1 p2
+moveInterval _           _           = Nothing
 
--- | Calculate the payoff for a single player, given his preferences.
-onePlayerPay :: [MusicMv] -> [[MusicMv]] -> [IntPreference] -> Float
-onePlayerPay [] _ _ = 0
-onePlayerPay _ [] _ = 0
-onePlayerPay (me:performance) others ps = 
-    foldr f 0 others + onePlayerPay performance (map tail others) ps
-    where f (m:ms) acc = case rmoveInterval me m of
-                            Nothing -> acc
-                            Just a  -> acc + intPref ps a
+-- | Calculate the payoff for a single player, given the moves of all the 
+-- other players and his preferences.
+myPayoff :: [MusicMv] -> [[MusicMv]] -> [IntPreference] -> Float
+myPayoff []              _      _     = 0
+myPayoff  _              []     _     = 0
+myPayoff (myMove:myPast) others prefs = 
+    foldr findIntervals 0 others + myPayoff myPast (map tail others) prefs
+    where findIntervals (hisMove:ms) pay = 
+            case moveInterval myMove hisMove of
+                    Just int -> pay + fromMaybe 0 (lookup int prefs)
+                    Nothing  -> pay 
 
 -- | Generate a payoff matrix from a list of the players' preferences and their
 -- respective scores, which are given by the game state.
 intervalPayoff :: [[IntPreference]] -> Performance -> Payoff
-intervalPayoff prefs performance = ByPlayer $ p [] (everyPlayer performance) prefs
-    where p _      []         _            = []
-          p _      _          []           = []
-          p before (me:after) (myPrefs:ps) = 
-            onePlayerPay (realization me) 
-                         (map realization (before ++ after)) myPrefs: 
-                            p (me:before) after ps
+intervalPayoff prefs performance = 
+    ByPlayer $ intervalPay [] (everyPlayer performance) prefs
+    where intervalPay _    []                 _            = []
+          intervalPay _     _                []            = []
+          intervalPay done (thisPlayer:next) (herPrefs:ps) = 
+            let herMoves       = realization thisPlayer
+                othersMoves    = map realization (done ++ next)
+                thisPlayersPay = myPayoff herMoves othersMoves herPrefs
+            in thisPlayersPay : intervalPay (thisPlayer:done) next ps
 
